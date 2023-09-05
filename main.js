f3f5423384def86a3a05519efe4c83e52944076c
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 02:12:08 +0530

    callback hell

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 console.log("Before");
 
-getUser(1, function (user) {
-    console.log(user);
+// Cosuming the callbacks 
+getUser(1, (user) => {
+    getRepositories(user.userName, (repos) => {
+        getCommits((commits) => {
+            console.log(commits);
+
+            // THIS IS CALLED CALLBACK HELL
+        })
+    })
 })
 
 console.log("After");
 
+// Creating callbacks
 function getUser(id, callback) {
     setTimeout(() => {
-        console.log('Reading user from database...');
+        console.log('Getting user object...');
         callback({ id: id, userName: 'redbull' });
     }, 2000);
 }
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Getting repositories array...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+function getCommits(commits) {
+    console.log('GitHub commit');
+}
+
+
+
+
+
+
+
+
+
+
 
 /* 
 
