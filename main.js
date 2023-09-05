465734328fa24761cbfc91ee3d0b2006ddae135a
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 01:44:38 +0530

    undefined nature of async code

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 console.log("Before");
 
-setTimeout(() => {
-    console.log('Reading user from database...');
-}, 2000);
+const user = getUser(1)
+console.log(user.id);
 
 console.log("After");
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading user from database...');
+        return { id: id, userName: 'redbull' }
+    }, 2000);
+}
+
+// OUTPUT
+    // Before
+    // undefined
+    // After
