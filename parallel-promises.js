6ec41357f49fcfd2880c2f491dea96dd9e104f7e
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 12:28:41 +0530

    parallel-promises

---
+++ parallel-promises.js
@@ -0,0 +1,24 @@
+// Parallel promises gets executed simataneously
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async operation 1...");
+        resolve(1);
+        // reject(new Error('something wrong.'))''
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log("Async operation 2...");
+        resolve(2)
+    }, 2000);
+});
+
+// Promise.all([p1, p2])
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error.message));
+
+Promise.race([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message));
