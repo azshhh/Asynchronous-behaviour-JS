e781953a5f565fbb1527a48d7b6a03b88432ad26
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 12:18:54 +0530

    replacing callback-approach with promise-approach

--- main.js
+++ main.js
@@ -1,42 +1,52 @@
-console.log("Before");
-
-// Asynchronous 
-getUser(1, (user) => {
-    getRepositories(user.userName, (repos) => {
-        // console.log(repos);  
-        getCommits((commits) => {
-            console.log(commits);
-
-            // THIS IS CALLED CALLBACK HELL
-        })
-    })
-})
-console.log("After");
-
-// Synchronous
+// Asynchronous approach
 // console.log("Before");
-// const user = getUser(1);
-// const repos = getRepositories(user.userName);
-// const commits = getCommits(repos[0]);
+// getUser(1, (user) => {
+//     getRepositories(user.userName, (repos) => {
+//         getCommits((commits) => {
+//             console.log(commits);
+//         })
+//     })
+// })
 // console.log("After");
 
+// Promise approach
+console.log("Before");
+getUser(1)
+.then(user => getRepositories(user.userName))
+.then(repos => getCommits(repos[0]))
+.then(commits => console.log(commits))
+.catch(error => console.log(error.message));
+console.log("After");
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Getting user object...');
-        callback({ id: id, userName: 'redbull' });
-    }, 2000);
+// getUser() returns a promise resolving user object, and to consume this object we use .then method
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting user object...');
+            resolve({ id: id, userName: 'redbull' });
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Getting repositories array...');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+// getRepositories() returns a prmoise resolving an array of repos of that userName, and consumed
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting repositories array...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+
 }
 
-function getCommits(commits) {
-    console.log('GitHub commit');
+// getCommits() returns a promise resolving an array of commit.
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['GitHub commit']);
+        }, 2000);
+    });
+
 }
 
 
@@ -47,6 +57,6 @@ OUTPUT
     After
     Getting user object...
     Getting repositories array...
-    GitHub commit
+    [ 'GitHub commit' ]
 
 */
